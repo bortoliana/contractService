ac811b5941d4526ae066ef8c3bbd78550a729edf
ana <analeticiadebortoli@gmail.com>
2022-10-06 19:51:08 -0300

    Project created

---
+++ src/services/ContracService.java
@@ -0,0 +1,41 @@
+package services;
+
+import entities.Contract;
+import entities.Installment;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class ContracService {
+
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContracService(OnlinePaymentService onlinePaymentService){
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, int months){
+        double basicQuota = contract.getTotalValue() / months;
+        //basicQuota = 200
+        for (int i = 1; i <= months; i++){
+            double updateQuota = basicQuota + onlinePaymentService.interest(basicQuota, i);
+            //updateQuota = 2
+            double fullQuota = updateQuota + onlinePaymentService.paymentFee(updateQuota);
+            //fullQuota = 206.04
+            Date dueDate = addMonths(contract.getDate(), i);
+            contract.getInstallments().add(new Installment(dueDate, fullQuota));
+
+
+        }
+    }
+
+    private Date addMonths(Date date, int N){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH, N);
+        return calendar.getTime();
+    }
+
+
+
+}
